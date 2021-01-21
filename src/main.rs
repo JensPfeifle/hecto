7c6ecb302c3908a39dea9509b49a4b4f025a1eec
Jens Pfeifle <jens@pfeifle.tech>
2021-01-21 21:39:01 +0100

    Lets start over ;)

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
-#![warn(clippy::all, clippy::pedantic, clippy::restriction)]
-#![allow(
-    clippy::missing_docs_in_private_items,
-    clippy::implicit_return,
-    clippy::shadow_reuse,
-    clippy::print_stdout,
-    clippy::wildcard_enum_match_arm,
-    clippy::else_if_without_else
-)]
-mod document;
-mod editor;
-mod filetype;
-mod highlighting;
-mod row;
-mod terminal;
-pub use document::Document;
-use editor::Editor;
-pub use editor::Position;
-pub use editor::SearchDirection;
-pub use filetype::FileType;
-pub use filetype::HighlightingOptions;
-pub use row::Row;
-pub use terminal::Terminal;
+#![warn(clippy::all)]
+
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
 
 fn main() {
-    Editor::default().run();
+    println!("Hello, world!");
+    // set terminal to raw mode, keep reference to stdout around
+    let _stdout = stdout().into_raw_mode().unwrap();
+    for b in io::stdin().bytes() {
+        let b = b.unwrap();
+        let c = b as char;
+
+        if c.is_control() {
+            // ASCI 0-31, 127
+            println!("{:?} \r", b)
+        } else {
+            // ASCI 32-126
+            println!("{:?} ({})\r", b, c)
+        }
+
+        if b == to_ctrl_byte('q') {
+            break;
+        }
+    }
 }
