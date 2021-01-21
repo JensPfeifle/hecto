d3417eb5540902d9f20b6396dc54c989d329d617
Jens Pfeifle <jens@pfeifle.tech>
2021-01-21 21:39:20 +0100

    Factor out 'die' function

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,32 @@ fn to_ctrl_byte(c: char) -> u8 {
     byte & 0b0001_1111
 }
 
+fn die(e: std::io::Error) {
+    panic!(e)
+}
+
 fn main() {
     println!("Hello, world!");
     // set terminal to raw mode, keep reference to stdout around
     let _stdout = stdout().into_raw_mode().unwrap();
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
+        match b {
+            Ok(b) => {
+                let c = b as char;
 
-        if c.is_control() {
-            // ASCI 0-31, 127
-            println!("{:?} \r", b)
-        } else {
-            // ASCI 32-126
-            println!("{:?} ({})\r", b, c)
-        }
+                if c.is_control() {
+                    // ASCI 0-31, 127
+                    println!("{:?} \r", b)
+                } else {
+                    // ASCI 32-126
+                    println!("{:?} ({})\r", b, c)
+                }
 
-        if b == to_ctrl_byte('q') {
-            break;
+                if b == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(err) => die(err),
         }
     }
 }
