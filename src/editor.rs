8ed2201366256ab61dac362051f698e84b9479c7
Jens Pfeifle <jens@pfeifle.tech>
2021-01-24 20:04:39 +0100

    We have documents!

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,11 @@
+use crate::Document;
+use crate::Row;
 use crate::Terminal;
 use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[derive(Default)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
@@ -12,6 +15,7 @@ pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
+    document: Document,
 }
 
 impl Editor {
@@ -32,7 +36,7 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::hide_cursor();
-        Terminal::position_cursor(&Position { x: 0, y: 0 });
+        Terminal::position_cursor(&Position::default());
         if self.should_quit {
             Terminal::clear_screen();
             println!("Goodbye.\r");
@@ -100,12 +104,23 @@ impl Editor {
         println!("{}\r", welcome_message);
     }
 
+    fn draw_row(&self, row: &Row) {
+        let start = 0;
+        let end = self.terminal.size().width as usize;
+        let row = row.render(start, end);
+        println!("{}\r", row);
+    }
+
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for row in 0..height - 1 {
+        for term_row in 0..height - 1 {
             Terminal::clear_current_line();
-            if row == height / 3 {
-                self.draw_welcome_message();
+            if let Some(row) = self.document.row(term_row as usize) {
+                self.draw_row(row);
+            } else if self.document.is_empty() {
+                if term_row == height / 3 {
+                    self.draw_welcome_message();
+                }
             } else {
                 println!("~\r");
             }
@@ -116,7 +131,8 @@ impl Editor {
         Editor {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
-            cursor_position: Position { x: 0, y: 0 },
+            cursor_position: Position::default(),
+            document: Document::open(),
         }
     }
 }
